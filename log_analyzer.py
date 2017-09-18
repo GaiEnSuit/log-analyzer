62d1a9ecaa129f035b32f656605fca643d054522
K Truong <truong.k77@gmail.com>
2017-09-17 17:09:47 -0700

    add functions for queries

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,9 +2,43 @@
 
 import psycopg2
 
-
+# Create psycopg2 objects
 conn = psycopg2.connect(dbname="news")
 cur = conn.cursor()
+
+# Query for most popular articles
 cur.execute('''
-    SELECT * FROM authors;
+    SELECT path, COUNT(path)
+    FROM log
+    WHERE status='200 OK' AND NOT path='/'
+    GROUP BY path
+    ORDER BY COUNT(path) DESC
+    LIMIT 3;
 ''')
+
+topArticles = cur.fetchall()
+
+# Query for most popular authors
+cur.execute('''
+    SELECT authors.name, COUNT(name) AS Views
+    FROM authors
+    RIGHT JOIN articles ON authors.id = articles.author
+    RIGHT JOIN log ON log.path LIKE '%' || articles.slug || '%'
+    WHERE log.Status='200 OK' AND name IS NOT NULL
+    GROUP BY name
+    ORDER BY views DESC;
+''')
+
+topAuthors = cur.fetchall()
+
+# Query for days where more than 1% of request resulted in an error
+cur.execute('''
+    SELECT CAST(time AS DATE), CAST(SUM(CAST(log.status!='200 OK' AS INT)) * 100 AS FLOAT / SUM(CAST(log.status IS NOT NULL AS INT)) FROM log GROUP BY CAST(time AS DATE);
+''')
+
+badDays = cur.fetchall()
+
+# Outputs File with results
+f = open("analysis.txt","w+")
+f.write(str(topArticles) + str(topAuthors) + str(badDays))
+f.close()
